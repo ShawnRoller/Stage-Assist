01e4d1a10fb09c4ef016f73fd8a644e4b2189f85
Shawn Roller <sroller@techstyle.com>
2023-01-06 07:37:09 -0500

    Initial Commit

---
+++ Stage Assist/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Stage Assist
+//
+//  Created by Shawn Roller on 1/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stage Assist/Stage_AssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stage_AssistApp.swift
+//  Stage Assist
+//
+//  Created by Shawn Roller on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Stage_AssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
