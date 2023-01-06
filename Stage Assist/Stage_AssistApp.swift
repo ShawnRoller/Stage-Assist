72d8da86c6a0fb4fe51600713570cbd4eae01cae
Shawn Roller <sroller@techstyle.com>
2023-01-06 08:07:29 -0500

    Adds better icons and refactors menu bar extra.

--- Stage Assist/Stage_AssistApp.swift
+++ Stage Assist/Stage_AssistApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct Stage_AssistApp: App {
+    @State private var stageManagerOn = false
+    private var icon: String {
+        return stageManagerOn ? "tablecells.fill" : "tablecells"
+    }
+    
     var body: some Scene {
-        MenuBarExtra(LocalizedStringKey(stringLiteral: "Stage Assist"), systemImage: "window.casement") {
-            MenuBar()
+        MenuBarExtra(LocalizedStringKey(stringLiteral: "Stage Assist"), systemImage: icon) {
+            VStack {
+                Toggle("Use Stage Manager", isOn: $stageManagerOn)
+                    .keyboardShortcut("s")
+                Toggle("Auto Toggle - coming soon", isOn: .constant(false))
+                    .disabled(true)
+                Divider()
+                Button("Quit") {
+                    NSApplication.shared.terminate(nil)
+                }.keyboardShortcut("q")
+            }
         }
     }
 }
